0e3e212ea8cd555e39e541c6e88aa6fbd9a3b8d6
Travis Dockter <travisdock@gmail.com>
2021-05-14 11:13:37 -0600

    Add room to msg_size for signature

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::sync::mpsc;
 use std::thread;
 
 const LOCAL: &str = "127.0.0.1:6000";
-const MSG_SIZE: usize = 128;
+const MSG_SIZE: usize = 256;
 
 fn sleep() {
     thread::sleep(::std::time::Duration::from_millis(100));
